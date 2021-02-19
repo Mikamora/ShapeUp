bfaeff97f04a04db05543016e6791114cdbd6974
Michael Richardson <mor7991@yahoo.com>
2021-02-19 14:20:11 -0600

    added comments and an offset

--- app.js
+++ app.js
@@ -12,27 +12,32 @@ const squBtn = document.querySelector(".square");
 const cirBtn = document.querySelector(".circle");
 const triBtn = document.querySelector(".triangle");
 
-let MAX = 520;
+let MAX = 600;
 let randomTri = 0;
 
 rectBtn.addEventListener("click", () => {
     let a = document.querySelector("#rectangle-height").value;
     let b = document.querySelector("#rectangle-width").value;
     new Rectangle(a, b);
+    document.querySelector("#rectangle-height").value = "";
+    document.querySelector("#rectangle-width").value = "";
 })
 squBtn.addEventListener("click", ()=> {
     let a = document.querySelector("#square-length").value;
     new Square(a);
+    document.querySelector("#square-length").value = "";
 })
 cirBtn.addEventListener("click", ()=>{
     let a = document.querySelector("#circle-radius").value;
     new Circle(a);
+    document.querySelector("#circle-radius").value ="";
 })
 triBtn.addEventListener("click", ()=>{
     MAX = 400;
     let a = document.querySelector("#triangle-height").value;
     new Triangle(a);
-    MAX = 520;
+    document.querySelector("#triangle-height").value ="";
+    MAX = 600;
 })
 
 class Shape{
@@ -43,19 +48,18 @@ class Shape{
         this.div.className = "shapes";
         this.div.style.width = `${width}px`;
         this.div.style.height = `${height}px`;
-        this.xVal = randomVal(0, MAX);
-        this.yVal = randomVal(0, MAX);
-        this.div.style.left = `${this.xVal}px`; //x coordinate
-        this.div.style.top = `${this.yVal}px`; //y coordinate
+        // this.xVal = randomVal(0, MAX);
+        // this.yVal = randomVal(0, MAX);
+        this.div.style.left = `${this.offset(this.height)}px`; //x coordinate
+        this.div.style.top = `${this.offset(this.width)}px`; //y coordinate
         this.updateColor();
         canvas.appendChild(this.div);
         this.dblclick();
         this.updatePanel();
 
     }
-    updateLocation(){
-        this.div.style.left = `${this.xVal}px`; //x coordinate
-        this.div.style.top = `${this.yVal}px`; //y coordinate
+    offset(offset){
+        return Math.floor(Math.random() * (600 - offset)); 
     }
     updateColor(){
         let randomColor = `rgb(${randomVal(0,255)}, ${randomVal(0,255)}, ${randomVal(0,255)}, ${(Math.random()* (1-.5)) + .5})`;
@@ -72,6 +76,7 @@ class Rectangle extends Shape{
     constructor(height, width){
         super(height, width);
         this.updatePanel();
+        //eventListener happens after this, so now it has these properties
     }
     updatePanel(){
         this.div.addEventListener("click", ()=>{
@@ -93,6 +98,8 @@ class Square extends Shape{
         super(height);
         this.width = height;
         this.div.style.width = `${height}px`;
+        this.div.style.left = `${this.offset(this.height)}px`; //x coordinate
+        this.div.style.top = `${this.offset(this.width)}px`; //y coordinate
         this.updatePanel();
     }
     updatePanel(){
@@ -113,6 +120,10 @@ class Square extends Shape{
 class Circle extends Shape{
     constructor(radius){
         super();
+        this.height = radius*2;   //could just put radius*2 in the super call to create a new width and height of radius*2
+        this.width = this.height;
+        this.div.style.left = `${this.offset(this.height)}px`; //x coordinate
+        this.div.style.top = `${this.offset(this.width)}px`; //y coordinate
         this.radius = radius;
         this.div.style.width = `${radius*2}px`;
         this.div.style.height = `${radius*2}px`;
@@ -127,19 +138,22 @@ class Circle extends Shape{
             widthTrack.textContent = `Width: ${this.width} pixels`;
             heightTrack.textContent = `Height: ${this.height} pixels`;
             radiusTrack.textContent = `Radius: ${this.radius} pixels`;
-            area.textContent = `Area: ${3.14 * (this.radius*this.radius)} pixels`;
-            perimeter.textContent = `Perimeter: ${3.14 * (2 * this.radius)} pixels`;
+            area.textContent = `Area: ${3.14 * Math.pow(this.radius, 2)} pixels`;
+            perimeter.textContent = `Perimeter: ${3.14 * (2 * this.radius)} pixels`; //you can also use Math.PI
         })
     }
 }
 
 class Triangle extends Shape{
     constructor(height){
-        super();
+        super(height);
         this.height = height;
+        this.width = height;
+        this.div.style.left = `${this.offset(this.height)}px`; //x coordinate
+        this.div.style.top = `${this.offset(this.width)}px`; //y coordinate
         this.div.style.height ="0";
         this.div.style.width ="0";
-        randomTri = randomVal(0, 5);
+        let randomTri = randomVal(0, 5);
         if(randomTri == 1){
             this.div.style.borderLeft = `${height}px solid transparent`;
             this.div.style.borderTop = `${height}px solid transparent`;
@@ -185,3 +199,5 @@ class Triangle extends Shape{
 function randomVal(min, max){
     return Math.floor(Math.random() * (max - min)) + min; //random num in between 2
 }
+
+//Why would you use an arrow function as opposed to a normal function in a class:       it will implicitly bind the context of the class to the function
